891acbe6d7e2a2c86b92a5cde5cc8c93309ddf05
prieta <sofiaboubion@gmail.com>
2024-02-29 10:38:21 -0800

    fixed the broken server

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,28 +1,17 @@
 const db = require('../config/connection');
-const { User, Thought } = require('../models');
+const { User, Tool } = require('../models');
 const userSeeds = require('./userSeeds.json');
-const thoughtSeeds = require('./thoughtSeeds.json');
+const toolSeeds = require('./toolSeeds.json');
 const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
   try {
-    await cleanDB('Thought', 'thoughts');
+    await cleanDB('Tool', 'tools');
 
     await cleanDB('User', 'users');
 
     await User.create(userSeeds);
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
-        {
-          $addToSet: {
-            thoughts: _id,
-          },
-        }
-      );
-    }
   } catch (err) {
     console.error(err);
     process.exit(1);
