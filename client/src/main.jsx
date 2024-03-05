2cb042c78f2372f80bb79cbcecad9e2432928a0d
Margaux <margaux.santos113@gmail.com>
2024-03-04 20:13:53 -0800

    board front end file name

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -27,7 +27,7 @@ const router = createBrowserRouter([
         element: <Signup />
       },// {
       //   path: '/',
-      //   element: <CommunityBoard/>
+      //   element: <Board/>
       // }
     ]
   },
