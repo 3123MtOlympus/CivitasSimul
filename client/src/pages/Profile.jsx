e1e16b0cd2c0c799380ec9686ebdcdd8ec9efe82
prieta <sofiaboubion@gmail.com>
2024-03-05 13:27:20 -0800

    adding modal to Profile page to addTools

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -18,30 +18,15 @@ const Profile = () => {
   useEffect(() => {
     console.log(data);
   }, [data])
-  // navigate to personal profile page if username is yours
-  // if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-  //   return <Navigate to="/" />;
-  // }
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
-  // if (!user?.username) {
-  //   return (
-  //     <h4>
-  //       You need to be logged in to see this. Use the navigation links above to
-  //       sign up or log in!
-  //     </h4>
-  //   );
-  // }
-
   return (
     <div >
       <div className="justify-center">
-
         <div className="card">
-
           <div>
             <h3>
               Viewing {`${user.username}'s`} profile
@@ -55,7 +40,6 @@ const Profile = () => {
               </h4>
             </div>
           </div>
-          
 
           {user.tools ? (
             <ToolList
@@ -65,6 +49,31 @@ const Profile = () => {
         </div>
 
       </div>
+
+      
+      <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
+
+      <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">Add a Tool</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              The form goes here
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Add Tool</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 };
